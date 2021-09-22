d390967c9bfc73929830b02e282f01930e8e7a55
Varun Joseph <63628481+varun-joseph@users.noreply.github.com>
2021-09-22 23:21:07 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,112 @@
+import "./styles.css";
+import { useState } from "react";
 
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
+import { Login } from "./components/loginPage";
+import { Register } from "./components/registerPage";
+import { ForgotPwdForm } from "./components/forgotPasswordForm";
+import { PasswordResetForm } from "./components/resetPasswordForm";
+import { Homepage } from "./components/homepage";
+
+import ClipLoader from "react-spinners/ClipLoader";
+import VpnLockIcon from "@material-ui/icons/VpnLock";
+
+export default function App() {
+  let [loading, setLoading] = useState(false);
+  const [user, setuser] = useState({});
+  const [popUp, setpopUp] = useState("popup message");
+  const [popOut, setpopOut] = useState("none");
+
+  return (
+    <>
+      <div
+        className="App"
+        style={{
+          filter: popOut === "block" ? "grayscale(70%)" : "grayscale(0)",
+
+          backgroundImage: `url(${require("./images/prfbg.jpg").default})`
+        }}
+      >
+        <Router>
+          <div id="header">
+            <div>
+              {" "}
+              <span>
+                <VpnLockIcon />
+              </span>
+              password reset
+            </div>
+
+            <div id="loadingDiv">
+              <ClipLoader color={"white"} loading={loading} size={45} />
+            </div>
+          </div>
+          <div></div>
+          <Switch>
+            <Route path="/login">
+              <Login
+                popup={setpopOut}
+                popupMsg={setpopUp}
+                setLoading={setLoading}
+                setuser={setuser}
+              />
+            </Route>
+            <Route path="/register">
+              <Register
+                popup={setpopOut}
+                popupMsg={setpopUp}
+                setLoading={setLoading}
+              />
+            </Route>
+            <Route path="/forgot">
+              <ForgotPwdForm
+                popup={setpopOut}
+                popupMsg={setpopUp}
+                setLoading={setLoading}
+              />
+            </Route>
+            <Route path="/reset">
+              <PasswordResetForm
+                popup={setpopOut}
+                popupMsg={setpopUp}
+                setLoading={setLoading}
+              />
+            </Route>
+            <Route path="/homepage">
+              <Homepage
+                popup={setpopOut}
+                popupMsg={setpopUp}
+                setLoading={setLoading}
+                user={user}
+              />
+            </Route>
+
+            <Route path="*">
+              <Login
+                popup={setpopOut}
+                popupMsg={setpopUp}
+                setLoading={setLoading}
+                setuser={setuser}
+              />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+
+      <div id="popup" style={{ display: popOut }}>
+        <button
+          onClick={() => {
+            setpopOut("none");
+          }}
+        >
+          X
+        </button>
+        <br></br>
+        <span>{popUp}</span>
+      </div>
+      <div></div>
+    </>
+  );
+}
